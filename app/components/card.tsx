2a07ba68cfb3edcce4fa4ae37e619ce62ce9113e
KirdesMF <cedgourville@gmail.com>
2023-03-01 02:12:07 +0100

    wip: cards

---
+++ app/components/card.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+
+type Status = "available" | "in-post" | "not-available";
+
+const colors = {
+  available: "bg-green",
+  "in-post": "bg-yellow",
+  "not-available": "bg-red",
+};
+
+type Props = {
+  status: Status;
+};
+
+export function CardStatus(props: Props) {
+  return (
+    <article className="from-neutral-900 to-black bg-gradient-to-b px-4 py-2">
+      <div className="flex items-center gap-4">
+        <span
+          className={clsx("block rounded-full w-2 h-2", colors[props.status])}
+        />
+        <p className="text-xs font-100 capitalize">{props.status}</p>
+      </div>
+    </article>
+  );
+}
+
+export function CardCurrentJob() {
+  return (
+    <article className="from-neutral-900 to-black bg-gradient-to-b px-4 py-2">
+      <div className="flex items-center gap-4"></div>
+    </article>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { ElasticGrid } from "~/components/canvas/elastic-grid";
+import { CardStatus } from "~/components/card";
 
 // TODO: move this to css
 const beforeStyles = [
@@ -30,22 +31,6 @@ export default function Index() {
         </h1>
       </section>
 
-      {/* <section className="px-8">
-        <div className="grid ma-auto mb-36 max-w-[min(100%,60ch)]">
-          <h2 className="mb-4 text-sm color-neutral">
-            Frontend developer with 5+ years of experience in building web.
-          </h2>
-
-          <p className="text-sm font-200 color-neutral">
-            Passionate about bringing designs to life, I love crafting beautiful
-            and intuitive user interfaces. With a keen eye for detail and a
-            focus on user experience, I try to bring my creativity and technical
-            skills to every project to deliver engaging and seamless digital
-            experiences.
-          </p>
-        </div>
-      </section> */}
-
       <section className="mb-36 px-8">
         <div className="custom-grid ma-auto max-w-[min(100%,60rem)]">
           <div
@@ -62,8 +47,6 @@ export default function Index() {
             className="radial-gradient grid-col-[2] grid-row-[1/-1]"
           />
 
-          <div className="grid-col-[3] grid-row-[3] flex flex-col items-center gap-8 px-4 py-24"></div>
-
           <div
             role="separator"
             aria-hidden
@@ -77,6 +60,10 @@ export default function Index() {
             aria-orientation="vertical"
             className="radial-gradient grid-col-[4] grid-row-[1/-1]"
           />
+
+          <div className="grid-col-[3] grid-row-[3] flex flex-col gap-8 px-4 py-2">
+            <CardStatus status="in-post" />
+          </div>
         </div>
       </section>
     </main>
