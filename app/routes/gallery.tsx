17c4f942d51f6ccc26090bb6106d1a245d6fbf7e
KirdesMF <cedgourville@gmail.com>
2023-03-02 16:25:11 +0100

    test: gpg key persistent

--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -1,7 +1,7 @@
 export default function Gallery() {
   return (
     <main className="grid min-h-[100vh] place-items-center">
-      <h1 className="text-8xl font-900 color-neutral">Gallery</h1>
+      <h1 className="text-8xl font-900 color-neutral">Gallery Page</h1>
     </main>
   );
 }
